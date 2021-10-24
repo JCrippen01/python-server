0481a2ba49ea2fdf9640c4a9b969f359a1982bc3
Justin <justin.crippen@gmail.com>
2021-10-23 19:14:46 -0500

    finished 9

--- employees/request.py
+++ employees/request.py
@@ -1,3 +1,6 @@
+import sqlite3
+import json
+from models import Employee
 EMPLOYEES = [
     {
       "id": 1,
@@ -195,25 +198,70 @@ EMPLOYEELOCATIONS = [
 ]
 
 def get_all_employees():
-    '''something'''
-    return EMPLOYEES
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.db") as conn:
 
-# Function with a single parameter
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.address,
+            e.location_id
+        FROM employee e
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        employees = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            employee = Employee(
+                row['id'], row['name'], row['address'], row['location_id'])
+
+            employees.append(employee.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(employees)
+
 
 def get_single_employee(id):
-    '''something'''
-    # Variable to hold the found employee, if it exists
-    requested_employee = None
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    # Iterate the employeeS list above. Very similar to the
-    # for..of loops you used in JavaScript.
-    for employee in EMPLOYEES:
-        # Dictionaries in Python use [] notation to find a key
-        # instead of the dot notation that JavaScript used.
-        if employee["id"] == id:
-            requested_employee = employee
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.address,
+            e.location_id
+        FROM employee e
+        WHERE e.id = ?
+        """, (id, ))
 
-    return requested_employee
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an employee instance from the current row
+        employee = Employee(data['id'], data['name'],
+                            data['address'], data['location_id'])
+
+        return json.dumps(employee.__dict__)
 
 def create_employee(employee):
     '''Apends the employees List: Next, in the employees/request.py module,
